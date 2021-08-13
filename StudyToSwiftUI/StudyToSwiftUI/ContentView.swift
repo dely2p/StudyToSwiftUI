1672f8b7d4ccf93b6fac65c35dddde28c8514d98
dely2p <lhyal2p@gmail.com>
2021-08-13 23:10:04 +0900

    make profile UI

--- StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
+++ StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            HStack {
+                Image("profile")
+                    .frame(width: 50.0, height: 50.0)
+                Text("홍길동")
+                    .padding()
+            }
+            HStack(alignment: .center, spacing: 0) {
+                Text("email: gildong.hong@doinglab.com")
+                    .padding()
+            }
+            HStack(alignment: .center, spacing: 0) {
+                Text("phone: 010-1234-5678")
+                    .padding()
+            }
+        }
     }
 }
 
