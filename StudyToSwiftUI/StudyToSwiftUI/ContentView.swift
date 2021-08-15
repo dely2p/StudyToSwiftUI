f10960ff5ee6bced3eb6ffdf455bb3d53b05f11d
dely2p <lhyal2p@gmail.com>
2021-08-15 23:45:37 +0900

    Text 설정

--- StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
+++ StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
@@ -9,22 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Image("profile")
-                    .frame(width: 50.0, height: 50.0)
-                Text("홍길동")
-                    .padding()
-            }
-            HStack(alignment: .center, spacing: 0) {
-                Text("email: gildong.hong@doinglab.com")
-                    .padding()
-            }
-            HStack(alignment: .center, spacing: 0) {
-                Text("phone: 010-1234-5678")
-                    .padding()
-            }
-        }
+        Text("가나다라마바사아자차카파타하abcdefghigklmnopqrstuvwxyz")
+            .lineLimit(nil)
+            .truncationMode(.head)
+            .font(.largeTitle)
+            .multilineTextAlignment(.center)
+            .foregroundColor(.blue)
+            .background(Color.yellow)
+            .lineSpacing(50)
     }
 }
 
