e6801aaa34d7cd5875393be28b8853419c0794c7
dely2p <lhyal2p@gmail.com>
2021-08-16 22:46:10 +0900

    글자 뒤에 이미지 배경 추가

--- StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
+++ StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Circle()
-            .fill(Color.blue)
-            .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        Text("apple")
+            .foregroundColor(.white)
+            .background(
+                Image("apple")
+                    .resizable()
+                    .frame(width: 300, height: 100, alignment: .center)
+            )
     }
 }
 
