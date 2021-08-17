7e260a765fcc5022b1d390de7ec0f8d31239866e
dely2p <lhyal2p@gmail.com>
2021-08-18 01:04:57 +0900

    다이나믹 리스트 구현

--- StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
+++ StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
@@ -7,20 +7,27 @@
 
 import SwiftUI
 
-struct FamilyRow: View {
+struct Family: Identifiable {
+    var id = UUID()
     var name: String
+}
+
+struct FamilyRow: View {
+    var family: Family
     var body: some View {
-        Text("Family: \(name)")
+        Text("Family: \(family.name)")
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        List {
-            FamilyRow(name: "피카츄")
-            FamilyRow(name: "라이츄")
-            FamilyRow(name: "파이리")
-        }
+        let first = Family(name: "피카츄")
+        let second = Family(name: "라이츄")
+        let third = Family(name: "파이리")
+        let families = [first, second, third]
+        return List(families, rowContent: { family in
+            FamilyRow(family: family)
+        })
     }
 }
 
