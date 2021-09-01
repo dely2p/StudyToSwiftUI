c279891654b34023a5ab7cf1bcd4db2ccaccf405
dely2p <lhyal2p@gmail.com>
2021-09-01 23:22:56 +0900

    선택한 Color Picker 보여주기 추가 구현

--- StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
+++ StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
@@ -11,11 +11,15 @@ struct ContentView: View {
     var colors = ["Red", "Green", "Blue", "Tartan"]
     @State var selectedColor = 0
     var body: some View {
-        Picker(selection: $selectedColor, label: Text("Choose a color"), content: {
-            ForEach(0..<colors.count) {
-                Text(self.colors[$0])
-            }
-        })
+        VStack {
+            Picker(selection: $selectedColor, label: Text("Choose a color"), content: {
+                ForEach(0..<colors.count) {
+                    Text(self.colors[$0])
+                }
+            })
+            
+            Text("You selected \(colors[selectedColor])")
+        }
     }
 }
 
