7ee961cab6c8fd9ac8d98f85f809db6c3be2ca4e
dely2p <lhyal2p@gmail.com>
2021-09-07 23:09:33 +0900

    Device 별로 Preview 보여주기 구현

--- StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
+++ StudyToSwiftUI/StudyToSwiftUI/ContentView.swift
@@ -8,21 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var showSheet = false
     var body: some View {
-        Button(action: {
-            self.showSheet = true
-        }, label: {
-            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
-        })
-        .actionSheet(isPresented: $showSheet, content: {
-            ActionSheet(title: Text("Title"), message: Text("message"), buttons: [.default(Text("Dismiss")), .cancel(Text("cancel"))])
-        })
+        Text("text")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+            ContentView()
+                .previewDevice(PreviewDevice(rawValue: "iPhone XS"))
+        }
     }
 }
